a3fd92a5ce1970bb9c608ae06216151908015290
MaryFerH <fer.hs19@gmail.com>
2020-03-18 22:36:47 -0500

    Se utilizo useState para el manejo de estados.

--- src/components/pages/Course.jsx
+++ src/components/pages/Course.jsx
@@ -1,53 +1,34 @@
-import React from 'react'
-
-const cursos = [
-    {
-        id: 1,
-        title: "React desde cero",
-        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTZWxq_DqhMbwm5yN1F3G3O7AU0r37vyj6MZOAGxj-7Gxq3Rr0R",
-        price: "20",
-        professor: "Fernando Mora"    
-    },
-    {
-        id: 2,
-        title: "HTML desde cero",
-        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTZWxq_DqhMbwm5yN1F3G3O7AU0r37vyj6MZOAGxj-7Gxq3Rr0R",
-        price: "20",
-        professor: "Fernando Mora"    
-    },
-    {
-        id: 3,
-        title: "JS desde cero",
-        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTZWxq_DqhMbwm5yN1F3G3O7AU0r37vyj6MZOAGxj-7Gxq3Rr0R",
-        price: "20",
-        professor: "Fernando Mora"    
-    },
-    {
-        id: 4,
-        title: "PHP desde cero",
-        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTZWxq_DqhMbwm5yN1F3G3O7AU0r37vyj6MZOAGxj-7Gxq3Rr0R",
-        price: "20",
-        professor: "Fernando Mora"    
-    }
-]
+import React,{useState} from 'react'
 
 const Course =({match})=>{
-    
-    const cursoActual = cursos.filter(c=>c.id ===parseInt(match.params.id))[0]
 
+    const [state, setstate] = useState({            
+            id: 2,
+            title: "HTML desde cero",
+            image: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTZWxq_DqhMbwm5yN1F3G3O7AU0r37vyj6MZOAGxj-7Gxq3Rr0R",
+            price: "20",
+            professor: "Fernando Mora"  
+    })
+    
+    const changeTitle = (text)=>{
+        setstate({
+            ...state,
+            title:text
+        })
+    }
     return (
         <div className="ed-grid m-grid-3">
         {
-            cursoActual 
+            state 
                 ? (
                     <>         
-                        <h1 className="m-cols-3">{cursoActual.title}</h1>
-                        <img className="m-cols-1" src={cursoActual.image} alt={cursoActual.title}></img>
+                        <h1 className="m-cols-3">{state.title}</h1>
+                        <img className="m-cols-1" src={state.image} alt={state.title}></img>
                         <p className="m-cols-2">Descripcion</p>
+                        <button onClick={changeTitle.bind(this,"Go desde Cero")}>Cambiar</button>
                     </>
                 ) :
-                    <h1>El curso no existe</h1>
-            
+                    <h1>El curso no existe</h1>  
         }
         </div>
            
